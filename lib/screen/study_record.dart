a6621708a79a78c05643a1e8762dc8aa17749054
Ryota Asai <ryota846538@outlook.jp>
2024-01-26 23:56:55 +0900

    make Timer

--- lib/screen/study_record.dart
+++ lib/screen/study_record.dart
@@ -3,53 +3,48 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class StudyRecord extends StatefulWidget{
+class StudyRecord extends StatefulWidget {
+  const StudyRecord({Key? key}) : super(key: key);
+
   @override
-  _StudyRecordState createState() => _StudyRecordState();
+  StudyRecordState createState() => StudyRecordState();
 }
 
-class _StudyRecordState extends State<StudyRecord>{
-  Timer _timer;
-  DateTime _time;
+class StudyRecordState extends State<StudyRecord> {
+  Timer? _timer;
+  DateTime _time = DateTime.utc(0, 0, 0);
 
   @override
-  void initState(){
-    _time = DateTime.utc(0, 0, 0);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text (
+            Text(
               DateFormat.Hms().format(_time),
-              style: const Theme.of(context).textTheme.headline2,
+              style: Theme.of(context).textTheme.headline2,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                FloatingActionButton(
-                  onPressed: () { // Stopボタンタップ時の処理
-                    if (_timer != null && _timer.isActive) _timer.cancel();
-                  },
+                const FloatingActionButton(
+                  onPressed: null, // Stopボタンタップ時の処理
                   child: Text("Stop"),
                 ),
                 FloatingActionButton(
-                  onPressed: (){
+                  onPressed: () {
                     _timer = Timer.periodic(
-                      Duration(seconds: 1),
+                      const Duration(seconds: 1),
                       (Timer timer) => setState(() {
-                        _time = _time.add(Duration(seconds: 1));
-                      })
+                        _time = _time.add(const Duration(seconds: 1));
+                      }),
                     );
                   },
-                  child: Text("Start"),
-                )
+                  child: const Text("Start"),
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
