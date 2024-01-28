0ca41da089832a64120c497e906ad1e350d5e192
Ryota Asai <ryota846538@outlook.jp>
2024-01-29 02:19:30 +0900

    chenge color

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -15,8 +15,8 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('がんばろう'),
-        backgroundColor: Colors.blue,
+        title: const Text('がんばろうね'),
+          backgroundColor: Colors.orange[300],
       ),
       body: Center(
         child: Container(
